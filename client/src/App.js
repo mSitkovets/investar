e224e0d5282279dbe8ad80fc72ab5a42677d2bfc
Hanniya Zohdi <hanniyazohdi@MacBook-Pro.local>
2021-01-09 12:04:23 -0800

    Started front-end page
    Started CSS and index page

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,48 @@
 import logo from './logo.png';
-import logotype from './logotype.png';
 import './App.css';
 
 import giveaway from './giveaway.svg';
+import logotype from './logotype.png';
 import buy from './buy.svg';
 import sell from './sell.svg';
 import yourstocks from './yourstocks.svg';
 
-import { Navbar, Jumbotron } from 'react-bootstrap'
+import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <Navbar bg="light" variant="light">
           <Navbar.Brand href="#home">
-            <img src={logotype} />
+            <img src={logotype} alt="Investar logo"/>
           </Navbar.Brand>
           <Nav className="mr-auto">
             <Nav.Link href="#index">Home</Nav.Link>
             <Nav.Link href="#dictionary">Dictionary</Nav.Link>
           </Nav>
         </Navbar>
-      </header>
-
-      <h2>Hello, fellow star</h2>
 
       <Container>
         <Row>
-          <Col><img src={giveaway} /></Col>
-          <Col>Your stock portfolio total is $890</Col>
-          <Col>Today you made $120</Col>
-          <Col>Which means your return rate is 4.3%</Col>
+          <Col><h2>Hello, fellow star! 👋</h2></Col>
+        </Row>
+      </Container>
+
+      <Container>
+        <Row>
+          <Col xs={4}><img src={giveaway} class="celebrating" alt="Illustration of girl celebrating!"/></Col>
+          <Col class="total">
+
+            <p>Your stock portfolio total is <br></br><h1>$890</h1></p>
+          </Col>
+          <Col class="profit">
+            <p>Today you made <br></br><h1>$120</h1></p>
+          </Col>
+          <Col class="rate">
+          
+            <p>Which means your return rate is <br></br><h1>4.3%</h1></p>
+          </Col>
         </Row>
       </Container>
 
