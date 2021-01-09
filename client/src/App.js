73f0c8f1df66e91494ccfaa0c145d212b4cbb62d
Hanniya Zohdi <hanniyazohdi@MacBook-Pro.local>
2021-01-09 12:11:53 -0800

    Stock Ticker Update
    Updated stock names and tickers based on hackathon sponsors!

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import buy from './buy.svg';
 import sell from './sell.svg';
 import yourstocks from './yourstocks.svg';
 
-import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap'
+import { Navbar, Nav, Container, Row, Col, Button } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
@@ -53,9 +53,57 @@ function App() {
 
           <Col><img src={yourstocks} />Your stocks!</Col>
 
-          <Col><img src={buy} />Buy</Col>
+          <Col><img src={buy} />Buy
+          <p>You have a $170 balance. If you want more money, you can sell some shares!</p>
+          <ul>
+            <li>$TSLA (Tesla)</li>
+            <li>$700</li>
+            <Button>Buy!</Button>
 
-          <Col><img src={sell} />Sell</Col>
+            <li>$BNS (Scotiabank)</li>
+            <li>$700</li>
+            <Button>Buy!</Button>
+
+            <li>$CM (CIBC)</li>
+            <li>$700</li>
+            <Button>Buy!</Button>
+
+            <li>$TD (TD Bank)</li>
+            <li>$700</li>
+            <Button>Buy!</Button>
+
+            <li>$ACN (Accenture)</li>
+            <li>$700</li>
+            <Button>Buy!</Button>
+          </ul>
+          
+          </Col>
+
+          <Col><img src={sell} />Sell
+          <p>Want to lock-in your profit? Or not feeling super hopeful about a company? Then sell!</p>
+            <ul>
+              <li>$TSLA (Tesla)</li>
+              <li>15 shares</li>
+              <Button>Sell!</Button>
+
+              <li>$BNS (Scotiabank)</li>
+              <li>7 shares</li>
+              <Button>Sell!</Button>
+
+              <li>$CM (CIBC)</li>
+              <li>14 shares</li>
+              <Button>Sell!</Button>
+
+              <li>$TD (TD Bank)</li>
+              <li>4 shares</li>
+              <Button>Sell!</Button>
+
+              <li>$ACN (Accenture)</li>
+              <li>1 share</li>
+              <Button>Sell!</Button>
+            </ul>
+          
+          </Col>
         </Row>
       </Container>
 
